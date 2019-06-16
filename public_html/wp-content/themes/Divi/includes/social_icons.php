a0106b5c4a9e6cd8e038ab29ab448d82f63d6550
Maros Janco <marosjanco@gmail.com>
2019-06-16 13:52:49 +0100

    git init repo with ftp client content and first beta version marosjanco.com

---
+++ public_html/wp-content/themes/Divi/includes/social_icons.php
@@ -0,0 +1,57 @@
+<ul class="et-social-icons">
+
+
+<li class="et-social-icon et-social-linkedin">
+<a href="https://www.linkedin.com/in/maro%C5%A1-jan%C4%8Do-565a3535/" class="icon">
+<span><?php esc_html_e( 'LinkedIn', 'Divi' ); ?></span>
+</a>
+</li>
+
+<?php if ( 'on' === et_get_option( 'divi_show_facebook_icon', 'on' ) ) : ?>
+	<li class="et-social-icon et-social-facebook">
+		<a href="<?php echo esc_url( et_get_option( 'divi_facebook_url', '#' ) ); ?>" class="icon">
+			<span><?php esc_html_e( 'Facebook', 'Divi' ); ?></span>
+		</a>
+	</li>
+<?php endif; ?>
+<?php if ( 'on' === et_get_option( 'divi_show_twitter_icon', 'on' ) ) : ?>
+	<li class="et-social-icon et-social-twitter">
+		<a href="<?php echo esc_url( et_get_option( 'divi_twitter_url', '#' ) ); ?>" class="icon">
+			<span><?php esc_html_e( 'Twitter', 'Divi' ); ?></span>
+		</a>
+	</li>
+<?php endif; ?>
+<?php if ( 'on' === et_get_option( 'divi_show_google_icon', 'on' ) ) : ?>
+	<li class="et-social-icon et-social-google-plus">
+		<a href="<?php echo esc_url( et_get_option( 'divi_google_url', '#' ) ); ?>" class="icon">
+			<span><?php esc_html_e( 'Google', 'Divi' ); ?></span>
+		</a>
+	</li>
+<?php endif; ?>
+<?php if ( 'on' === et_get_option( 'divi_show_rss_icon', 'on' ) ) : ?>
+<?php
+	$et_rss_url = '' !== et_get_option( 'divi_rss_url' )
+		? et_get_option( 'divi_rss_url' )
+		: get_bloginfo( 'rss2_url' );
+?>
+	<li class="et-social-icon et-social-rss">
+		<a href="<?php echo esc_url( $et_rss_url ); ?>" class="icon">
+			<span><?php esc_html_e( 'RSS', 'Divi' ); ?></span>
+		</a>
+	</li>
+<?php endif; ?>
+
+<!-- <li class="et-social-icon et-social-500px">
+<a href="https://500px.com/marosjanco" class="icon">
+<span><?php esc_html_e( '500PX', 'Divi' ); ?></span>
+</a>
+</li>
+ -->
+
+<li class="et-social-icon">
+<a href="https://500px.com/marosjanco" class="icon">
+<i class="fab fa-500px"></i>
+</a>
+</li>
+
+</ul>
